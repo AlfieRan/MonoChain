32e9f9d1f20b1288521fbef5273b274b45abffc8
Alfie Ranstead <alfie.ranstead@outlook.com>
2022-03-24 10:19:31 +0000

    setup basics for monorepo

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+**Alfie Ranstead's A Level Project - A JSON Oriented Blockchain**
+
+This git repo uses a monorepo in order to keep the various sections of the project in one repository, to view individual parts of the project, visit the "/packages" folder.
+
+The packages listed in the packages folder are as follows:
+ * Documentation - the documentation for the whole project
+ * Node - The code that runs on a computer intending to use it's storage or cpu power alongside it's network connection in order to support the blockchain and recieve a percentage of transactions that flow through it in return.
+ * Webportal - An example website that allows Users of the blockchain to see the data they own on the blockchain and send transactions to other Users on the blockchain.
---
+++ packages/webportal/src/pages/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/provider";
+import { extendTheme } from "@chakra-ui/react";
+import { SWRConfig } from "swr";
+// import { fetcher } from "../utils/fetcher";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        color: "textColour",
+        background: "#7F7FD5" /* fallback for old browsers */,
+      },
+    },
+  },
+  colors: {
+    background: "#A9BCD0",
+    subHover: "#ccced7",
+    subColour: "white",
+    textColour: "black",
+    confirmColourMain: "#2545e3",
+    confirmColourHover: "#505bec",
+    confirmColourClick: "#1f289a",
+  },
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SWRConfig
+      value={{
+        // fetcher(url) {
+        //   return fetcher("GET", url).then((res) => res.data);
+        // },
+        refreshInterval: 5 * 1000,
+      }}
+    >
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SWRConfig>
+  );
+}
