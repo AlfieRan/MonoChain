ca67d3ccf3715ad5026d7504b41f7ce9aa9c39ca
AlfieRan <alfie.ranstead@outlook.com>
2022-06-04 13:14:36 +0100

    updated node software to say hello on a http server

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,3 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const api = express();
+api.use(express.json());
+api.use(cors({
+    origin: false,
+    credentials: true,
+}))
+
+const port = 8001 // this should be changed to abide by the config file at a later date.
 const message = "Hello, world!";
 
-console.log(message);
+api.get("/", (req: any, res: any) => {
+    res.send(message);
+});
+
+
+(async () => {
+    api.listen(port || 8001, () => {
+        console.log(`server started at http://localhost:${port}`);
+    });
+})().catch((e) => {
+    console.error(e);
+});
