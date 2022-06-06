aac4cfc67f5f4c55f7419fad711653b8accb058e
AlfieRan <alfie.ranstead@outlook.com>
2022-06-06 09:21:15 +0100

    commenting

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,25 +1,32 @@
+// import required web server modules
 import express from "express";
 import cors from "cors";
 
+// create the web server and configure it to allow all cors connections
+// and that connections should include credentials if they have any
 const api = express();
 api.use(express.json());
-api.use(cors({
+api.use(
+  cors({
     origin: false,
     credentials: true,
-}))
+  })
+);
 
-const port = 8001 // this should be changed to abide by the config file at a later date.
+// setup some initial constants
+const port = 8001; // this should be changed to abide by the config file at a later date.
 const message = "Hello, world!";
 
+// set the message to be delivered to any connection request
 api.get("/", (req: any, res: any) => {
-    res.send(message);
+  res.send(message);
 });
 
-
+// start the server and log in the console if anything has gone wrong.
 (async () => {
-    api.listen(port || 8001, () => {
-        console.log(`server started at http://localhost:${port}`);
-    });
+  api.listen(port || 8001, () => {
+    console.log(`server started at http://localhost:${port}`);
+  });
 })().catch((e) => {
-    console.error(e);
+  console.error(e);
 });
