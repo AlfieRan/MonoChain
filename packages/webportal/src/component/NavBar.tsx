f7584f978b9fe38be3d4fb9f1e8e268e0e7b9e08
AlfieRan <alfie.ranstead@outlook.com>
2022-06-23 00:14:54 +0100

    started setting up mobile navbar

--- packages/webportal/src/component/NavBar.tsx
+++ packages/webportal/src/component/NavBar.tsx
@@ -5,10 +5,12 @@ import {
     Link,
     Collapse,
     Text,
+    css,
 } from "@chakra-ui/react";
 import { Links } from "../utils/types";
 import DevNavigation from "./Dev-navigation";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import useWindowSize from "../utils/window";
 
 const LinkHover: Interpolation<{}> = {
     background: "",
@@ -17,8 +19,21 @@ const LinkHover: Interpolation<{}> = {
 
 const NavBar = () => {
     const [devShowing, setDevShowing] = useState<boolean>(false);
+
+    const window = useWindowSize();
+    const breakpoint = 700;
+
     const [mobile, setMobile] = useState<boolean>(true);
     const [showingMenu, setShowingMenu] = useState<boolean>(!mobile);
+
+    useEffect(() => {
+        if ((window.width || 0) < breakpoint) {
+            setMobile(true);
+        } else if ((window.width || 0) > breakpoint) {
+            setMobile(false);
+        }
+        console.log(window.width, mobile, (window.width || 0) < breakpoint);
+    }, [window.width]);
 
     const LinkMarginAmount = 6;
     const Links: Links[] = [
@@ -35,74 +50,87 @@ const NavBar = () => {
 
     return (
         <>
-            <Flex
-                w={"full"}
-                bg={"rgba(17,17,17,0.1)"}
-                justifyContent={"center"}
-                fontSize={"xl"}
-                px={5}
-                pb={2}
-                pt={4}
-            >
-                {Links.map((data) => {
-                    if (data.type === "component") {
-                        return (
-                            <Flex flexDir={"column"} key={data.name}>
-                                <Button
-                                    bg={""}
-                                    my={0}
-                                    mx={LinkMarginAmount}
-                                    p={0}
-                                    _hover={LinkHover}
-                                    _active={LinkHover}
-                                    onClick={() => {
-                                        data.state.change(!data.state.value);
-                                    }}
-                                >
-                                    <Text
-                                        fontSize={"xl"}
-                                        fontWeight={"normal"}
-                                        h={"full"}
-                                        w={"full"}
-                                        lineHeight={"base"}
+            <div hidden={mobile}>
+                <Flex css={NavBarCss}>
+                    {Links.map((data) => {
+                        if (data.type === "component") {
+                            return (
+                                <Flex flexDir={"column"} key={data.name}>
+                                    <Button
+                                        bg={""}
+                                        my={0}
+                                        mx={LinkMarginAmount}
+                                        p={0}
+                                        _hover={LinkHover}
+                                        _active={LinkHover}
+                                        onClick={() => {
+                                            data.state.change(
+                                                !data.state.value
+                                            );
+                                        }}
                                     >
+                                        <Text
+                                            fontSize={"xl"}
+                                            fontWeight={"normal"}
+                                            h={"full"}
+                                            w={"full"}
+                                            lineHeight={"base"}
+                                        >
+                                            {data.name}
+                                        </Text>
+                                    </Button>
+                                </Flex>
+                            );
+                        } else {
+                            return (
+                                <Link
+                                    key={data.name}
+                                    href={data.href}
+                                    mx={LinkMarginAmount}
+                                    isExternal={!!data.external}
+                                    _hover={LinkHover}
+                                >
+                                    <Text fontSize={"xl"} h={"full"} w={"full"}>
                                         {data.name}
                                     </Text>
-                                </Button>
-                            </Flex>
-                        );
-                    } else {
-                        return (
-                            <Link
-                                key={data.name}
-                                href={data.href}
-                                mx={LinkMarginAmount}
-                                isExternal={!!data.external}
-                                _hover={LinkHover}
-                            >
-                                <Text fontSize={"xl"} h={"full"} w={"full"}>
-                                    {data.name}
-                                </Text>
-                            </Link>
-                        );
+                                </Link>
+                            );
+                        }
+                    })}
+                </Flex>
+                {Links.filter((item) => item.type === "component").map(
+                    (data) => {
+                        if (data.type === "component") {
+                            return (
+                                <Collapse
+                                    key={data.name}
+                                    in={data.state.value}
+                                    dir="top"
+                                >
+                                    <data.comp />
+                                </Collapse>
+                            );
+                        }
                     }
-                })}
-            </Flex>
-            {Links.filter((item) => item.type === "component").map((data) => {
-                if (data.type === "component") {
-                    return (
-                        <Collapse
-                            key={data.name}
-                            in={data.state.value}
-                            dir="top"
-                        >
-                            <data.comp />
-                        </Collapse>
-                    );
-                }
-            })}
+                )}
+            </div>
+            <div hidden={!mobile}>
+                <Flex css={NavBarCss}>
+                    <Text>Home</Text>
+                </Flex>
+            </div>
         </>
     );
 };
 
 export default NavBar;
+
+const NavBarCss = css({
+    w: "full",
+    bg: "rgba(17,17,17,0.1)",
+    justifyContent: "center",
+    fontSize: "xl",
+    px: 5,
+    pb: 2,
+    pt: 4,
+});
