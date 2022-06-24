518f6c8289d06b4bbd5021e7a5dd83c39f2b0b3a
AlfieRan <alfie.ranstead@outlook.com>
2022-06-24 12:24:10 +0100

    Updated index

--- packages/webportal/src/component/NavBar.tsx
+++ packages/webportal/src/component/NavBar.tsx
@@ -1,11 +1,14 @@
 import {
-    Button,
-    Flex,
-    Interpolation,
-    Link,
-    Collapse,
-    Text,
-    css,
+  Button,
+  Flex,
+  Interpolation,
+  Link,
+  Collapse,
+  Text,
+  css,
+  Center,
+  Slide,
+  Box,
 } from "@chakra-ui/react";
 import { Links } from "../utils/types";
 import DevNavigation from "./Dev-navigation";
@@ -13,124 +16,155 @@ import React, { useState, useEffect } from "react";
 import useWindowSize from "../utils/window";
 
 const LinkHover: Interpolation<{}> = {
-    background: "",
-    color: "#4343dc",
+  background: "",
+  color: "#4343dc",
 };
 
 const NavBar = () => {
-    const [devShowing, setDevShowing] = useState<boolean>(false);
+  const [devShowing, setDevShowing] = useState<boolean>(false);
 
-    const window = useWindowSize();
-    const breakpoint = 700;
+  const window = useWindowSize();
+  const breakpoint = 700;
 
-    const [mobile, setMobile] = useState<boolean>(true);
-    const [showingMenu, setShowingMenu] = useState<boolean>(!mobile);
+  const [mobile, setMobile] = useState<boolean>(true);
+  const [showingMenu, setShowingMenu] = useState<boolean>(!mobile);
 
-    useEffect(() => {
-        if ((window.width || 0) < breakpoint) {
-            setMobile(true);
-        } else if ((window.width || 0) > breakpoint) {
-            setMobile(false);
-        }
-        console.log(window.width, mobile, (window.width || 0) < breakpoint);
-    }, [window.width]);
+  const [shownInfo, setShownInfo] = useState<boolean>(true);
+  const [showingInfo, setShowingInfo] = useState<boolean>(false);
 
-    const LinkMarginAmount = 6;
-    const Links: Links[] = [
-        { name: "Home", href: "/" },
-        { name: "Learn More", href: "/info" },
-        {
-            name: "Developers",
-            comp: DevNavigation,
-            state: { change: setDevShowing, value: devShowing },
-            type: "component",
-        },
-        { name: "Wallet", href: "/wallet" },
-    ];
+  useEffect(() => {
+    setShownInfo(localStorage.getItem("shownInfo") === "true");
+    setShowingInfo(!shownInfo);
+  }, [shownInfo]);
 
-    return (
-        <>
-            <div hidden={mobile}>
-                <Flex css={NavBarCss}>
-                    {Links.map((data) => {
-                        if (data.type === "component") {
-                            return (
-                                <Flex flexDir={"column"} key={data.name}>
-                                    <Button
-                                        bg={""}
-                                        my={0}
-                                        mx={LinkMarginAmount}
-                                        p={0}
-                                        _hover={LinkHover}
-                                        _active={LinkHover}
-                                        onClick={() => {
-                                            data.state.change(
-                                                !data.state.value
-                                            );
-                                        }}
-                                    >
-                                        <Text
-                                            fontSize={"xl"}
-                                            fontWeight={"normal"}
-                                            h={"full"}
-                                            w={"full"}
-                                            lineHeight={"base"}
-                                        >
-                                            {data.name}
-                                        </Text>
-                                    </Button>
-                                </Flex>
-                            );
-                        } else {
-                            return (
-                                <Link
-                                    key={data.name}
-                                    href={data.href}
-                                    mx={LinkMarginAmount}
-                                    isExternal={!!data.external}
-                                    _hover={LinkHover}
-                                >
-                                    <Text fontSize={"xl"} h={"full"} w={"full"}>
-                                        {data.name}
-                                    </Text>
-                                </Link>
-                            );
-                        }
-                    })}
+  function hideInfo() {
+    setShowingInfo(false);
+    setShownInfo(true);
+    localStorage.setItem("shownInfo", "true");
+  }
+
+  useEffect(() => {
+    if ((window.width || 0) < breakpoint) {
+      setMobile(true);
+    } else if ((window.width || 0) > breakpoint) {
+      setMobile(false);
+    }
+    console.log(window.width, mobile, (window.width || 0) < breakpoint);
+  }, [window.width]);
+
+  const LinkMarginAmount = 6;
+  const Links: Links[] = [
+    { name: "Home", href: "/" },
+    { name: "Learn More", href: "/info" },
+    {
+      name: "Developers",
+      comp: DevNavigation,
+      state: { change: setDevShowing, value: devShowing },
+      type: "component",
+    },
+    { name: "Wallet", href: "/wallet" },
+  ];
+
+  return (
+    <Box w={"100%"}>
+      <div hidden={mobile}>
+        <Flex css={NavBarCss}>
+          {Links.map((data) => {
+            if (data.type === "component") {
+              return (
+                <Flex flexDir={"column"} key={data.name}>
+                  <Button
+                    bg={""}
+                    my={0}
+                    mx={LinkMarginAmount}
+                    p={0}
+                    _hover={LinkHover}
+                    _active={LinkHover}
+                    onClick={() => {
+                      data.state.change(!data.state.value);
+                    }}
+                  >
+                    <Text
+                      fontSize={"xl"}
+                      fontWeight={"normal"}
+                      h={"full"}
+                      w={"full"}
+                      lineHeight={"base"}
+                    >
+                      {data.name}
+                    </Text>
+                  </Button>
                 </Flex>
-                {Links.filter((item) => item.type === "component").map(
-                    (data) => {
-                        if (data.type === "component") {
-                            return (
-                                <Collapse
-                                    key={data.name}
-                                    in={data.state.value}
-                                    dir="top"
-                                >
-                                    <data.comp />
-                                </Collapse>
-                            );
-                        }
-                    }
-                )}
-            </div>
-            <div hidden={!mobile}>
-                <Flex css={NavBarCss}>
-                    <Text>Home</Text>
-                </Flex>
-            </div>
-        </>
-    );
+              );
+            } else {
+              return (
+                <Link
+                  key={data.name}
+                  href={data.href}
+                  mx={LinkMarginAmount}
+                  isExternal={!!data.external}
+                  _hover={LinkHover}
+                >
+                  <Text fontSize={"xl"} h={"full"} w={"full"}>
+                    {data.name}
+                  </Text>
+                </Link>
+              );
+            }
+          })}
+        </Flex>
+        {Links.filter((item) => item.type === "component").map((data) => {
+          if (data.type === "component") {
+            return (
+              <Collapse key={data.name} in={data.state.value} dir="top">
+                <data.comp />
+              </Collapse>
+            );
+          }
+        })}
+      </div>
+      <div hidden={!mobile}>
+        <Flex css={NavBarCss}>
+          <Text>Home</Text>
+        </Flex>
+      </div>
+      <Slide direction="bottom" in={showingInfo} style={{ zIndex: 10 }}>
+        <Center
+          bg={"#5b5be1"}
+          color={"white"}
+          p={3}
+          fontSize={"lg"}
+          flexWrap={"wrap"}
+        >
+          <Text flexDir={"column"} maxW={"700px"} textAlign={"left"}>
+            Please be Aware That This Website is in Early Development and is not
+            a Full Representation of the Final Project.
+          </Text>
+          <Button
+            onClick={() => {
+              hideInfo();
+            }}
+            bg={"rgba(0,0,0,0.2)"}
+            _hover={{ bg: "rgba(0,0,0,0.4)" }}
+            _active={{ bg: "rgba(0,0,0,0.2)" }}
+            mx={2}
+          >
+            <Text>Close</Text>
+          </Button>
+        </Center>
+      </Slide>
+    </Box>
+  );
 };
 
 export default NavBar;
 
 const NavBarCss = css({
-    w: "full",
-    bg: "rgba(17,17,17,0.1)",
-    justifyContent: "center",
-    fontSize: "xl",
-    px: 5,
-    pb: 2,
-    pt: 4,
+  w: "full",
+  bg: "rgba(17,17,17,0.1)",
+  justifyContent: "center",
+  fontSize: "xl",
+  px: 5,
+  pb: 2,
+  pt: 4,
 });
--- packages/webportal/src/pages/index.tsx
+++ packages/webportal/src/pages/index.tsx
@@ -1,19 +1,52 @@
-import { AspectRatio, Center, Flex, Spacer, Text } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Center,
+  Flex,
+  Grid,
+  GridItem,
+  Link,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
 
 const index = () => {
+  const gridItems: gridItemProps[] = [
+    {
+      link: "/wallet",
+      title: "Wallet",
+      subtitle: "Create and manage your wallet.",
+    },
+    {
+      link: "/info",
+      title: "Learn More",
+      subtitle: "Learn more about the MonoChain.",
+    },
+    {
+      link: "/download",
+      title: "Setup a Node",
+      subtitle:
+        "Download a node to support the MonoChain in exchange for a fee.",
+    },
+    {
+      link: "https://github.com/AlfieRan/A-Level-Project",
+      title: "View the Code",
+      subtitle: "View the open source code for the default Node and webportal.",
+    },
+  ];
+
   return (
-    <Flex flexDir="column">
+    <Flex flexDir="column" pb={"100px"}>
       <AspectRatio maxH="5%" ratio={5 / 1}>
-        <Image src="/index/nodes-banner.png" layout="fill" />
+        <Image src="/index/nodes-banner.png" layout="fill" priority />
       </AspectRatio>
       <Flex
         w={"100vw"}
         h={"full"}
         flexDir={"column"}
         color={"black"}
-        px={10}
+        px={2}
         maxW="1500px"
         m="auto"
       >
@@ -37,7 +70,12 @@ const index = () => {
         <Spacer />
         <Center flexDir="row" w="100%" my="5%" flexWrap={"wrap"}>
           <Flex px="1%" maxW="600px">
-            <Flex fontSize={"lg"} flexDir="column" p="3%" textAlign={"right"}>
+            <Flex
+              fontSize={"lg"}
+              flexDir="column"
+              p="3%"
+              textAlign={["center", "right"]}
+            >
               <Text fontSize={"2xl"} mb="10px" fontWeight={"semibold"}>
                 What is Proof of Trust?
               </Text>
@@ -55,11 +93,65 @@ const index = () => {
             </AspectRatio>
           </Flex>
         </Center>
+        <Spacer />
+        <Center flexDir={"column"} width={"100%"}>
+          <Text fontSize={"2xl"} mb="10px" fontWeight={"semibold"}>
+            Use the Monochain
+          </Text>
+          <Center w={"100%"} overflow={"hidden"} textAlign={"center"}>
+            <Grid gridTemplateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]}>
+              {gridItems.map((item) => (
+                <GridItem
+                  key={item.title}
+                  m={2}
+                  maxW={"100%"}
+                  w={["4xs", "md"]}
+                  h={["2xs", "xs"]}
+                >
+                  <Link
+                    href={item.link}
+                    _hover={{ textDecoration: "none" }}
+                    w={"100%"}
+                    h={"100%"}
+                  >
+                    <Center
+                      w={"100%"}
+                      h={"100%"}
+                      p={2}
+                      bg={"rgba(100,100,255,0.2)"}
+                      transitionDuration={"0.15s"}
+                      _hover={{
+                        bg: "rgba(100,100,255,0.3)",
+                        transform: "scale(1.03)",
+                      }}
+                      _active={{
+                        bg: "rgba(100,100,255,0.1)",
+                        transform: "scale(0.97)",
+                      }}
+                      borderRadius={"xl"}
+                      flexDir={"column"}
+                    >
+                      <Text fontSize={"2xl"} fontWeight={"semibold"}>
+                        {item.title}
+                      </Text>
+                      <Text fontSize={"lg"}>{item.subtitle}</Text>
+                    </Center>
+                  </Link>
+                </GridItem>
+              ))}
+            </Grid>
+          </Center>
+        </Center>
       </Flex>
     </Flex>
   );
 };
 
-// Images are stored in the /public/index folder
+type gridItemProps = {
+  link: string;
+  title: string;
+  subtitle: string;
+};
 
+// Images are stored in the /public/index folder
 export default index;
