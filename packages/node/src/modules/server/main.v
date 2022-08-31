6404ccabd8a0356b36f6da3eef31c4079b406d3b
AlfieRan <alfie.ranstead@outlook.com>
2022-08-31 21:38:13 +0100

    moved all handshake processes into one file

--- packages/node/src/modules/server/main.v
+++ packages/node/src/modules/server/main.v
@@ -1,9 +1,12 @@
 module server
-import vweb
-import json
-import time
+
+// internal imports
 import configuration
-import cryptography
+
+// external imports
+import vweb
+import time
+
 
 struct App {
 	vweb.Context
@@ -14,7 +17,7 @@ pub fn start(config configuration.UserConfig) {
 	api := go vweb.run(&App{config: config}, config.port) // start server on a new thread
 	
 	time.sleep(2 * time.second) // wait to make sure server is up
-	server.ping("https://nano.monochain.network", config) // ping running node using handshake to verify cryptography is working
+	server.start_handshake("https://nano.monochain.network", config) // ping running node using handshake to verify cryptography is working
 	api.wait()	// bring server process back to main thread
 }
 
@@ -22,41 +25,3 @@ pub fn (mut app App) index() vweb.Result {
 	return app.text("Hello, World!")
 }
 
-
-['/pong'; post]
-pub fn (mut app App) pong() vweb.Result {
-	body := app.req.data
-
-	req_parsed := json.decode(PingRequest, body) or {
-		eprintln("Incorrect data supplied to /pong/")
-		return app.server_error(403)
-	}
-
-	config := configuration.get_config()
-	keys := cryptography.get_keys(config.key_path)
-
-	println("Received pong request from node with public key: $req_parsed.ping_key")
-
-	// with this version of the node software all messages should be time objects
-	time := time.parse(req_parsed.message) or {
-		eprintln("Incorrect time format supplied to /pong/:req by node claiming to be $req_parsed.ping_key")
-		// this is where we would then store a record of the node's claimed public key
-		// after doing so, send back another handshake and if the node really is that node,
-		// then store a slight negative grudge
-		// otherwise, store the ip address of the node and blacklist it for a while
-		return app.server_error(403)
-	}
-
-	// time was okay, so store a slight positive grudge
-	println("Time parsed correctly as: $time")
-
-	res := PongResponse{
-		pong_key: keys.pub_key
-		ping_key: req_parsed.ping_key
-		message: req_parsed.message
-		signature: keys.sign(req_parsed.message.bytes())
-	}
-
-	data := json.encode(res)
-	return app.text(data)
-}
